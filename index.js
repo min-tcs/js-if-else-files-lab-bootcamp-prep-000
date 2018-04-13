a97bacbf966a5741ffc12fcf3947012b98afe277
Minakshi Yadav <min07ster@gmail.com>
2018-04-13 17:00:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ var height = "74"
 if(name =="Susan")
 {
   console.log("the name is correct");
-}
-// Don't worry about this
-module.exports = { name, height
-} else{
+}else{
   console.log("Expected the name to be Susan");
 }
 
+// Don't worry about this
+module.exports = { name, height
+} 
